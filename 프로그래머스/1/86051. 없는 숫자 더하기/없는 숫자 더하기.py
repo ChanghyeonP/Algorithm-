72e16f9837a50d5f09aac48a7776346dd1d229fb
창현 <71171046+ChanghyeonP@users.noreply.github.com>
2024-04-30 16:22:17 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+   # answer = -1
+   # arr = []
+   # for i in range(0, 10):
+   #     if i not in numbers:
+   #         arr.append(i)
+   # answer = sum(arr)
+    return 45 - sum(numbers)
